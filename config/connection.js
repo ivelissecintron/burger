76f5dee337eb1913d9249badf2eabb12a9f73c33
Ivelisse Cintron <33500791+ivelissecintron@users.noreply.github.com>
2018-06-27 10:43:40 -0500

    updated files

--- config/connection.js
+++ config/connection.js
@@ -5,7 +5,7 @@ var connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
   user: "root",
-  password: "fullstackdev@123",
+  password: "Cleveland@23!",
   database: "burgers_db"
 });
 
